237dfed5300cd432bb93c5854e927a2bf509a15d
yelp1022@163.com <Administrator@172.18.77.27>
2016-04-11 14:51:02 +0800

    commit IAreaDao

--- jxqd/jxqd-dao/src/main/java/com/antox/dao/IAreaDao.java
+++ jxqd/jxqd-dao/src/main/java/com/antox/dao/IAreaDao.java
@@ -5,10 +5,7 @@ import java.util.List;
 import com.antox.annotation.MyBatisRepository;
 import com.antox.entity.ConstArea;
 /**
- * @author 66(987927981@qq.com)<br>
- * @date 2016-01-05
- * 
- * @version 2.0
+ *
  */
 @MyBatisRepository
 public interface IAreaDao {  
